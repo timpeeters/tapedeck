6dc79cc74c89217823b91946702029904cfd4fd9
Tim Peeters <timpeeters@users.noreply.github.com>
2018-03-05 13:22:41 +0100

    Add a private constructor to hide the implicit public one

--- src/main/java/io/tapedeck/util/StringUtils.java
+++ src/main/java/io/tapedeck/util/StringUtils.java
@@ -1,6 +1,9 @@
 package io.tapedeck.util;
 
 public abstract class StringUtils {
+    private StringUtils() {
+    }
+
     public static boolean isEmpty(Object string) {
         return string == null || "".equals(string);
     }
