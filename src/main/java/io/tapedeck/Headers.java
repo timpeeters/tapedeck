edaf36053ad1ae16bac735376c8ce30391141203
Tim Peeters <timpeeters@users.noreply.github.com>
2018-03-05 13:25:50 +0100

    Add a private constructor to hide the implicit public one

--- src/main/java/io/tapedeck/Headers.java
+++ src/main/java/io/tapedeck/Headers.java
@@ -3,4 +3,7 @@ package io.tapedeck;
 public class Headers {
     public static final String ACCEPT = "Accept";
     public static final String CONTENT_TYPE = "Content-Type";
+
+    private Headers() {
+    }
 }
