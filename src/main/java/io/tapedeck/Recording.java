1445731c354b1c5e8718339b7cb52def6154dcfe
Tim Peeters <tim.peeters@me.com>
2018-03-05 17:39:35 +0100

    Remove unused variables.

--- src/main/java/io/tapedeck/Recording.java
+++ src/main/java/io/tapedeck/Recording.java
@@ -10,11 +10,6 @@ public class Recording {
     }
 
     public Response getResponse() {
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-
         return response;
     }
 
