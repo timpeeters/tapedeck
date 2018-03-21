da3f4a82e18e60afc75c7993fed0954e0f135ecf
Tim Peeters <tim.peeters@me.com>
2018-03-21 15:45:32 +0100

    Add hashCodeEquality test.

--- src/test/java/io/tapedeck/HashCodeContractTester.java
+++ src/test/java/io/tapedeck/HashCodeContractTester.java
@@ -7,8 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 public interface HashCodeContractTester<T> {
     T getInstance();
 
+    /**
+     * Whenever hashCode is invoked on the same object more than once during an execution of a Java application, the
+     * hashCode method must consistently return the same value.
+     */
     @Test
     default void hashCodeConsistency() {
-        assertThat(getInstance().hashCode()).isEqualTo(getInstance().hashCode()).as("hashCode should be consistent");
+        T a = getInstance();
+
+        assertThat(a.hashCode()).isEqualTo(a.hashCode()).as("hashCode should be consistent");
+    }
+
+    @Test
+    default void hashCodeEquality() {
+        T a = getInstance();
+        T b = getInstance();
+
+        assertThat(a.hashCode()).isEqualTo(b.hashCode()).as("hashCode should be equal");
     }
 }
