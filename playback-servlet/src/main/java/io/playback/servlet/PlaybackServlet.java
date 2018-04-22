8bb31dcc8d26d98e1a6a09276ff2a737a65a2d9d
Tim Peeters <tim.peeters@me.com>
2018-04-22 11:08:26 +0200

    Add initial servlet implementation.

---
+++ playback-servlet/src/main/java/io/playback/servlet/PlaybackServlet.java
@@ -0,0 +1,16 @@
+package io.playback.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class PlaybackServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+}
