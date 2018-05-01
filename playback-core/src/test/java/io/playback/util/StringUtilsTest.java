a2f8c5d33dfdda21de51f2c63cfe9ad1308c46dd
Tim Peeters <tim.peeters@me.com>
2018-05-01 09:57:08 +0200

    Use parameterized test.

--- playback-core/src/test/java/io/playback/util/StringUtilsTest.java
+++ playback-core/src/test/java/io/playback/util/StringUtilsTest.java
@@ -1,22 +1,27 @@
 package io.playback.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringUtilsTest {
-    @Test
-    public void isEmpty_null() {
-        assertThat(StringUtils.isEmpty(null)).isTrue();
+    @MethodSource("isEmptyValues")
+    @ParameterizedTest(name = "{0}")
+    void isEmpty(ArgumentsAccessor args) {
+        assertThat(StringUtils.isEmpty(args.getString(1)))
+                .as(args.getString(0))
+                .isEqualTo(args.getBoolean(2));
     }
 
-    @Test
-    public void isEmpty_emptyString() {
-        assertThat(StringUtils.isEmpty("")).isTrue();
-    }
-
-    @Test
-    public void isEmpty_false() {
-        assertThat(StringUtils.isEmpty("a")).isFalse();
+    static Stream<Arguments> isEmptyValues() {
+        return Stream.of(
+                Arguments.of("null should return true", null, true),
+                Arguments.of("empty string should return true", "", true),
+                Arguments.of("non empty string should return false", "a", false));
     }
 }
