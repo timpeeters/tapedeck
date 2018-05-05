337075a54595c4cbbe042887c20b6b56006c64e4
Tim Peeters <tim.peeters@me.com>
2018-05-05 20:55:00 +0200

    Add test coverage of Request.

--- playback-core/src/test/java/io/playback/RequestTest.java
+++ playback-core/src/test/java/io/playback/RequestTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class RequestTest {
+public class RequestTest implements EqualsContractTester<Request>, HashCodeContractTester<Request> {
+    @Override
+    public Request getInstance() {
+        return Request.get("/").header(Headers.ACCEPT, "text/html").queryParam("id", "1").build();
+    }
+
     @Test
     public void get_invalidUri() {
         assertThatThrownBy(() -> Request.get(" ")).isInstanceOf(IllegalArgumentException.class);
